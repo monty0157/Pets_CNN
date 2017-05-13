520b7036c01e3c38d70e0a346fd35962073602df
Kasper Moerch Zederkof <kaspermz@hotmail.com>
2017-05-13 13:10:26 +0200

    Added image preprocessing

--- cnn.py
+++ cnn.py
@@ -22,5 +22,32 @@ def build_model():
     model.add(Dense(units = 128, activation = 'relu'))
     model.add(Dense(units = 1, activation = 'sigmoid'))
     
+    model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    
     return model
 
+#CREATING TEST SET AND TRAINING SET
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   rotation_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+train_dataset = train_datagen.flow_from_directory('dataset/training_set',
+                                                  batch_size = 32,
+                                                  class_mode = 'binary',
+                                                  target_size = (64,64))
+
+test_dataset = test_datagen.flow_from_directory('dataset/test_set',
+                                                  batch_size = 32,
+                                                  class_mode = 'binary',
+                                                  target_size = (64,64))
+
+build_model().fit_generator(train_dataset,
+                            samples_per_epoch = 8000,
+                            nb_epoch = 25,
+                            validation_data = test_dataset,
+                            nb_val_samples = 2000)
